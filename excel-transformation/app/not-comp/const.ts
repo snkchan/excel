1c564b6d209480aae8b7363f8a00335d50d14ece
snkchan <144839872+snkchan@users.noreply.github.com>
2024-11-27 19:52:56 +0900

    기초 퍼블리싱 완료

--- excel-transformation/app/not-comp/const.ts
+++ excel-transformation/app/not-comp/const.ts
@@ -12,5 +12,47 @@ export const keyMap: Record<string, string> = {
 }
 
 export const ROW_TITLE_KEY_ARR = Object.keys(keyMap)
-
 export const ROW_TITLE_VALUE_ARR = Object.values(keyMap)
+
+/** 품종관리 상수 선언 */
+
+export const MainBreedMap = {
+  "이탈리안 라이그라스": "Italian_Ryegrass",
+  호밀: "Rye",
+  연맥: "Oats",
+}
+
+export const MainBreedKeyArr = Object.keys(MainBreedMap)
+export const ManBreedValueArr = Object.values(MainBreedMap)
+
+export const ItalianRyegrassSubMap = {
+  플로리다80: "Florida80",
+  그린팜: "GreenFarm",
+  윈터호크: "WinterHawk",
+  ED: "ED",
+  코위너리: "Cowinner",
+  메가플러스: "MegaPlus",
+  크레던스: "Credence",
+  마샬: "Marshall",
+  FrostProof: "FrostProof",
+  테트라스타: "TetraStar",
+}
+export const ItalianRyegrassKeyArr = Object.keys(ItalianRyegrassSubMap)
+export const ItalianRyegrassValueArr = Object.values(ItalianRyegrassSubMap)
+
+export const RyeSubMap = {
+  윈터그레이져70: "WinterGrazer70",
+  엘본: "Elbon",
+  프리마: "Prima",
+}
+
+export const OatsSubMap = {
+  스완: "Swan",
+  카스유: "Cassue",
+}
+
+export const SubBreedMap = {
+  Italian_Ryegrass: Object.values(ItalianRyegrassSubMap),
+  Rye: Object.values(RyeSubMap),
+  Oats: Object.values(OatsSubMap),
+}
--- excel-transformation/app/test/page.tsx
+++ excel-transformation/app/test/page.tsx
@@ -1,3 +1,5 @@
+import { ItalianRyegrassKeyArr, MainBreedKeyArr } from "../not-comp/const"
+
 export default function Test() {
   return (
     <div className="w-full h-full flex justify-center pt-6">
@@ -50,59 +52,136 @@ function SubTitle() {
 
 function Table() {
   return (
-    <table className="table-fixed border-collapse border-2 border-black w-full h-[600px] max-w-[794px]">
-      <thead>
-        <tr className="grid grid-cols-[20px_450px_170px_150px] h-[54.74px]  ">
-          <td className="table-border text-center h-[54.74px]">구분</td>
-          <td className="grid-rows-2 w-full h-[28.37px] border-collapse ">
-            <div className="text-center table-border w-full tracking-[.5rem] ">
-              공 급 내 역
-            </div>
-            <div className="grid grid-cols-[175px_125px_auto]  h-[28.37px]">
-              <div className="text-center table-border w-full ">초종명</div>
-              <div className="text-center table-border w-full ">숙기</div>
-              <div className="text-center table-border w-full">품종명</div>
-            </div>
-          </td>
-          <td className="table-border grid grid-rows-2">
-            <div className="text-center table-border w-full">공 급 수 량</div>
-            <div className="grid grid-cols-2">
-              <div className="text-center table-border w-full">포</div>
-              <div className="text-center table-border w-full">K G</div>
-            </div>
-          </td>
-          <td className="table-border flex items-center justify-center">
-            선 하 증 권
-          </td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="grid grid-cols-[20px_450px_170px_150px] h-[492.66px]  ">
-          <td className="h-full gap-8 flex flex-col justify-center table-border">
-            <span>사</span>
-            <span>료</span>
-          </td>
-          <td className="grid grid-cols-[300px_auto]">
-            <div></div>
-            <div></div>
-          </td>
-        </tr>
-      </tbody>
-      {/* <tbody>
-        {Array.from({ length: 18 }).map((_, rowIndex) => (
-          <tr key={rowIndex} className="w-full">
-            {Array.from({ length: 7 }).map((_, colIndex) => (
-              <td
-                key={colIndex}
-                className="border border-gray-500 text-center h-[27.37px]"
-              >
-                {rowIndex + 1},{colIndex + 1}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody> */}
+    <table className="table-fixed border-collapse border-2 border-black w-full h-[627px] max-w-[794px]">
+      <TableHeader />
+      <TableBody />
+      <TableFooter />
     </table>
+  )
+}
+
+function TableHeader() {
+  return (
+    <thead>
+      <tr className="grid grid-cols-[20px_450px_170px_150px] h-[66px]  ">
+        <td className="table-border center h-[66px]">구분</td>
+        <td className="grid-rows-2 w-full h-[66px] border-collapse ">
+          <div className="center table-border w-full tracking-[.5rem] h-[33px] ">
+            공 급 내 역
+          </div>
+          <div className="grid grid-cols-[175px_125px_auto] h-[33px]">
+            <div className="center table-border w-full ">초종명</div>
+            <div className="center table-border w-full ">숙기</div>
+            <div className="center table-border w-full">품종명</div>
+          </div>
+        </td>
+        <td className="table-border grid grid-rows-2">
+          <div className="center table-border w-full">공 급 수 량</div>
+          <div className="grid grid-cols-2">
+            <div className="center table-border w-full">포</div>
+            <div className="center table-border w-full">K G</div>
+          </div>
+        </td>
+        <td className="table-border flex items-center justify-center">
+          선 하 증 권
+        </td>
+      </tr>
+    </thead>
+  )
+}
+
+function TableBody() {
+  return (
+    <tbody>
+      <tr className="grid grid-cols-[20px_auto]">
+        <td className="table-border flex flex-col justify-center h-[528px]">
+          사료
+        </td>
+        <td className="grid grid-rows-[396px_66px_66px]">
+          <ItalianRyegrass />
+          <Rye />
+          <Oats />
+        </td>
+      </tr>
+    </tbody>
+  )
+}
+
+type TableItemPT = {
+  breedType?: string | undefined
+}
+
+function TableItem({ breedType }: TableItemPT) {
+  return (
+    <div className="grid grid-cols-[150px_85px_85px_150px] h-[33px]">
+      <p className="table-border">{breedType}</p>
+      <p className="table-border"></p>
+      <p className="table-border"></p>
+      <p className="table-border"></p>
+    </div>
+  )
+}
+
+function ItalianRyegrass() {
+  return (
+    <div className="grid grid-cols-[300px_auto]">
+      <div className="center table-border">{`${MainBreedKeyArr[0]}20Kg`}</div>
+      <div>
+        {Array.from({ length: 12 }, (_, idx) => (
+          <TableItem
+            breedType={ItalianRyegrassKeyArr[idx]}
+            key={ItalianRyegrassKeyArr[idx]}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function Rye() {
+  return (
+    <div className="grid grid-cols-[175px_125px_auto]">
+      <div className="center table-border">{`${MainBreedKeyArr[1]} 20Kg`}</div>
+      <div className="grid grid-rows-2">
+        <p className="center table-border">조/중생</p>
+        <p className="center  table-border">만생</p>
+      </div>
+      <div>
+        <TableItem />
+        <TableItem />
+      </div>
+    </div>
+  )
+}
+
+function Oats() {
+  return (
+    <div className="grid grid-cols-[175px_125px_auto]">
+      <div className="center table-border">{`${MainBreedKeyArr[2]} 20Kg`}</div>
+      <div className="grid grid-rows-2">
+        <p className="center table-border">조/중생</p>
+        <p className="center  table-border">만생</p>
+      </div>
+      <div>
+        <TableItem />
+        <TableItem />
+      </div>
+    </div>
+  )
+}
+
+function TableFooter() {
+  return (
+    <tfoot>
+      <tr className="grid grid-cols-[470px_170px_150px] h-[33px]">
+        <td className="center table-border">합계</td>
+        <td className="grid grid-cols-2">
+          <div className="table-border center"></div>
+          <div className="table-border center"></div>
+        </td>
+        <td className="table-border"></td>
+      </tr>
+    </tfoot>
   )
 }
 
