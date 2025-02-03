78b982a258949738ccc1261c1e0674e7adfe73a5
snkchan <144839872+snkchan@users.noreply.github.com>
2025-02-04 00:54:22 +0900

    v2 layout 정리 & 엑셀업로드 완료

--- excel-transformation/app/hold/index.tsx
+++ excel-transformation/app/hold/index.tsx
@@ -7,10 +7,8 @@ export const convertEngToKor = (title: string) => {
 
 export function DeliveryInstrctionHeaderTitle() {
   return (
-    <div className="text-5xl font-bold w-full flex items-center justify-center pb-2 ">
-      <div className="text-red-500 w-fit h-fit  p-3 rounded-full border-4 border-gray-700 ">
-        main 출고지시서
-      </div>
+    <div className="text-5xl font-bold w-full flex items-center justify-center   py-4  ">
+      <div className="text-red-500/80 w-fit h-fit  ">Main 출고지시서</div>
     </div>
   )
 }
@@ -18,10 +16,10 @@ export function DeliveryInstrctionHeaderTitle() {
 export function RowTitle() {
   return (
     <div className="w-full h-fit">
-      <div className="grid h-fit w-full grid-cols-[repeat(4,0.3fr)_repeat(2,0.8fr)_1fr_repeat(3,0.25fr)_0.1fr]">
+      <div className="grid h-fit w-full grid-cols-[repeat(4,minmax(80px,0.3fr))_repeat(2,minmax(150px,0.8fr))_minmax(200px,1fr)_repeat(3,minmax(60px,0.25fr))_minmax(30px,0.1fr)]">
         {ROW_TITLE_VALUE_ARR.map((title, idx) => (
           <div className="font-bold border border-gray-200" key={idx}>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center bg-gray-100">
               {convertEngToKor(title)}
             </span>
           </div>
--- excel-transformation/app/v2/page.tsx
+++ excel-transformation/app/v2/page.tsx
@@ -2,7 +2,9 @@
 import * as XLSX from "xlsx"
 import { ConvertedExcelDataType, ExcelDataType, summaryType } from "../types"
 import { convertOrderData } from "./(business-logic)"
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
+import { convertEngToKor, DeliveryInstrctionHeaderTitle } from "../hold"
+import { ROW_TITLE_VALUE_ARR } from "../not-comp/const"
 
 export default function V2() {
   const [excelData, setExcelData] = useState<Array<ConvertedExcelDataType>>([]) // 초기값을 null로 설정
@@ -39,9 +41,81 @@ export default function V2() {
 
     reader.readAsArrayBuffer(file)
   }
+
   return (
-    <div>
-      <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
+    <div className="w-full h-dvh flex items-center justify-center pt-20">
+      <div className="w-[90dvw] h-dvh ">
+        {/* 수정된 부분 */}
+        <UpLoadExcelBtn handleFileUpload={handleFileUpload} />
+        {excelData.length > 0 && <ExcelDataSheet excelData={excelData} />}
+      </div>
+    </div>
+  )
+}
+
+type UpLoadExcelBtnPT = {
+  handleFileUpload: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+// 엑셀 파일업로드 버튼 컴포넌트
+function UpLoadExcelBtn({ handleFileUpload }: UpLoadExcelBtnPT) {
+  return (
+    <div className="relative w-40 h-12 bg-red-200 rounded-lg">
+      <input
+        type="file"
+        accept=".xlsx, .xls"
+        onChange={handleFileUpload}
+        className="absolute inset-0 opacity-0 cursor-pointer"
+      />
+      <button className="w-full h-full bg-blue-500 text-white rounded-lg flex items-center justify-center cursor-pointer">
+        파일 선택
+      </button>
+    </div>
+  )
+}
+
+type ExcelDataSheetPT = {
+  excelData: Array<ConvertedExcelDataType>
+}
+
+// 엑셀 표 컴포넌트
+function ExcelDataSheet({ excelData }: ExcelDataSheetPT) {
+  return (
+    <>
+      <DeliveryInstrctionHeaderTitle />
+      <div>
+        <RowTitle />
+        {excelData.map((info, idx) => (
+          <div
+            className="hover:bg-gray-100 group cursor-pointer grid h-fit w-full grid-cols-[minmax(60px,1fr)_minmax(80px,1.3fr)_minmax(40px,0.7fr)_minmax(40px,0.7fr)_minmax(50px,0.85fr)_minmax(140px,2fr)_minmax(260px,3.5fr)_minmax(60px,1fr)_minmax(150px,2fr)_minmax(80px,1.3fr)]"
+            key={idx}
+          >
+            {ROW_TITLE_VALUE_ARR.map((key, keyIdx) => (
+              <div className="font-bold border border-gray-200 " key={keyIdx}>
+                <span className="w-full h-fit flex items-center justify-center text-center text-gray-600  break-all p-2 text-sm group-hover:text-black group-hover:text-sm ">
+                  {info[key as keyof ConvertedExcelDataType]}
+                </span>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+function RowTitle() {
+  return (
+    <div className="w-full h-fit">
+      <div className="grid h-fit w-full grid-cols-[minmax(60px,1fr)_minmax(80px,1.3fr)_minmax(40px,0.7fr)_minmax(40px,0.7fr)_minmax(50px,0.85fr)_minmax(140px,2fr)_minmax(260px,3.5fr)_minmax(60px,1fr)_minmax(150px,2fr)_minmax(80px,1.3fr)] text-sm">
+        {ROW_TITLE_VALUE_ARR.map((title, idx) => (
+          <div className="font-bold border border-gray-200" key={idx}>
+            <span className="flex items-center justify-center break-all bg-gray-100 p-1">
+              {convertEngToKor(title)}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
